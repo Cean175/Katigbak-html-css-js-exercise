4a7eb01703685da966cab1e92e21d0af165321a2
cean <cean_darius_katigbak@dlsl.edu.ph>
2024-09-03 11:38:39 +0800

    added html js and css

---
+++ Katigbak.js
@@ -0,0 +1,9 @@
+
+function changePortfoliocolor() {
+    document.getElementById('portfolio').style.backgroundColor = '#E9967A';
+}
+
+function changePortfolioBgColor() {
+    console.log("Changing portfolio background color"); // Debugging line
+    document.getElementById('portfolio').style.backgroundColor = '#d1e7dd';
+}
